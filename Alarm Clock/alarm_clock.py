f46511224685f8ad9f438ac4bd6e51f6839969a0
Umar Ansari <uans125@gmail.com>
2024-12-08 11:47:56 +0100

    changed to alarm.wav

--- Alarm Clock/alarm_clock.py
+++ Alarm Clock/alarm_clock.py
@@ -1,24 +1,8 @@
 from playsound import playsound
 import time
 
-
 CLEAR = "\033[2J"
 CLEAR_AND_RETURN = "\033[H"
-
-# def alarm(seconds):
-#     time_elapsed = 0
-
-#     print(CLEAR)
-#     while time_elapsed < seconds:
-#         time.sleep(1)
-#         time_elapsed += 1
-
-#         time_left = seconds - time_elapsed
-#         minutes_left = time_left // 60
-#         seconds_left = time_left % 60
-
-#         print(f"\r{CLEAR_AND_RETURN}Alarm in: {minutes_left:02d}:{seconds_left:02d}",end="")
-        
 
 def alarm(secs):
 	for x in range(secs,-1,-1):
@@ -35,5 +19,5 @@ total_seconds = minutes * 60 + seconds
 
  
 alarm(total_seconds)
-playsound("alarm.mp3")
+playsound("alarm.wav")
 
